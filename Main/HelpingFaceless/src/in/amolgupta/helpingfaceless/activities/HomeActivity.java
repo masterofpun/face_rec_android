5189e38d20d3e569b665b11da1608cfa03d019f3
amol <amolgupta1989@gmail.com>
2013-10-27 14:01:07 +0530

    Initial Login Flow

--- Main/HelpingFaceless/src/in/amolgupta/helpingfaceless/activities/HomeActivity.java
+++ Main/HelpingFaceless/src/in/amolgupta/helpingfaceless/activities/HomeActivity.java
@@ -1,16 +1,20 @@
 package in.amolgupta.helpingfaceless.activities;
 
 import in.amolgupta.helpingfaceless.R;
+import in.amolgupta.helpingfaceless.SetupActivity;
+import in.amolgupta.helpingfaceless.common.Constants;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
 public class HomeActivity extends FragmentActivity {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		launchLoginScreen();
+
 	}
 
 	@Override
@@ -20,4 +24,16 @@ public class HomeActivity extends FragmentActivity {
 		return true;
 	}
 
+	private void launchLoginScreen() {
+		/*
+		 * Read prefrences to get Logged in state.
+		 */
+		if (Constants.mIsLoggedIN==false) {
+			Intent homeIntent = new Intent(this, SetupActivity.class);
+			homeIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(homeIntent);
+			finish();
+		}
+
+	}
 }
--- Main/HelpingFaceless/src/in/amolgupta/helpingfaceless/activities/UpoadForm.java
+++ Main/HelpingFaceless/src/in/amolgupta/helpingfaceless/activities/UpoadForm.java
@@ -16,49 +16,64 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+/**
+ * @author amol
+ * 
+ */
 public class UpoadForm extends Activity {
 	private static final int TAKE_PICTURE = 85;
 	private Uri imageUri;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_upload_form);
 		takePhoto();
-	
-	}
-	public void takePhoto() {
-	    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-	    File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
-	    intent.putExtra(MediaStore.EXTRA_OUTPUT,
-	            Uri.fromFile(photo));
-	    imageUri = Uri.fromFile(photo);
-	    startActivityForResult(intent, TAKE_PICTURE);
+
 	}
 
+	/**
+	 * Intent to open the camera to capture photo
+	 */
+	public void takePhoto() {
+		Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+		File photo = new File(Environment.getExternalStorageDirectory(),
+				"Pic.jpg");
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+		imageUri = Uri.fromFile(photo);
+		startActivityForResult(intent, TAKE_PICTURE);
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
+	 */
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    super.onActivityResult(requestCode, resultCode, data);
-	    switch (requestCode) {
-	    case TAKE_PICTURE:
-	        if (resultCode == Activity.RESULT_OK) {
-	            Uri selectedImage = imageUri;
-	            getContentResolver().notifyChange(selectedImage, null);
-	            ImageView imageView = (ImageView) findViewById(R.id.img_upload_form);
-	            ContentResolver cr = getContentResolver();
-	            Bitmap bitmap;
-	            try {
-	                 bitmap = android.provider.MediaStore.Images.Media
-	                 .getBitmap(cr, selectedImage);
+		super.onActivityResult(requestCode, resultCode, data);
+		switch (requestCode) {
+		case TAKE_PICTURE:
+			/*
+			 * Handle camera output
+			 */
+			if (resultCode == Activity.RESULT_OK) {
+				Uri selectedImage = imageUri;
+				getContentResolver().notifyChange(selectedImage, null);
+				ImageView imageView = (ImageView) findViewById(R.id.img_upload_form);
+				ContentResolver cr = getContentResolver();
+				Bitmap bitmap;
+				try {
+					bitmap = android.provider.MediaStore.Images.Media
+							.getBitmap(cr, selectedImage);
 
-	                imageView.setImageBitmap(bitmap);
-	                Toast.makeText(this, selectedImage.toString(),
-	                        Toast.LENGTH_LONG).show();
-	            } catch (Exception e) {
-	                Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
-	                        .show();
-	                Log.e("Camera", e.toString());
-	            }
-	        }
-	    }
+					imageView.setImageBitmap(bitmap);
+					Toast.makeText(this, selectedImage.toString(),
+							Toast.LENGTH_LONG).show();
+				} catch (Exception e) {
+					Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
+							.show();
+					Log.e("Camera", e.toString());
+				}
+			}
+		}
 	}
 }
